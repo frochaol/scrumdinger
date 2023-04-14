eb959ed13f635eb8deb6abe6a642f8287ad84054
Fernando Rocha Olivera <frochaolivera18@hotmail.com>
2023-04-13 20:40:36 -0500

    feat: [Scrumdinger] add codable conformance

--- MeetingView.swift
+++ MeetingView.swift
@@ -12,9 +12,6 @@ struct MeetingView: View {
     @Binding var scrum: DailyScrum
     @StateObject var scrumTimer = ScrumTimer()
     
-    private var player: AVPlayer { AVPlayer.sharedDingPlayer
-    }
-    
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 16.0)
@@ -32,10 +29,6 @@ struct MeetingView: View {
         .foregroundColor(scrum.theme.accentColor)
         .onAppear {
             scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
-            scrumTimer.speakerChangedAction = {
-                player.seek(to: .zero)
-                player.play()
-            }
             scrumTimer.startScrum()
         }
         .onDisappear {
