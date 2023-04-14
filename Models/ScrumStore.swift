be2e477e7a45eed98932048e1ac13b24f626f669
Fernando Rocha Olivera <frochaolivera18@hotmail.com>
2023-04-13 20:45:07 -0500

    feat: [Scrumdinger] create a data store

---
+++ Models/ScrumStore.swift
@@ -0,0 +1,20 @@
+//
+//  ScrumStore.swift
+//  Scrumdinger
+//
+//  Created by Fernando Rocha Olivera on 13/04/23.
+//
+
+import SwiftUI
+
+class ScrumStore: ObservableObject {
+    @Published var scrums: [DailyScrum] = []
+    
+    private static func fileURL() throws -> URL {
+        try FileManager.default.url(for: .documentDirectory,
+                                    in: .userDomainMask,
+                                    appropriateFor: nil,
+                                    create: false)
+        .appendingPathComponent("scrums.data")
+    }
+}
